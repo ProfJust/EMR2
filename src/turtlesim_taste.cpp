e6966276b8d5a19b4427b01e6576ece5cb223bd1
ProfJust <olaf.just@w-hs.de>
2024-03-11 12:58:01 +0100

    Vorgabe

--- src/turtlesim_taste.cpp
+++ src/turtlesim_taste.cpp
@@ -1,5 +1,9 @@
-#include <cstdio>
+// emr2 - P02 - TurtleSim WASD-Steurung
+// Vorgabe - zu vervollständigen
+// OJ am 11.03.2024
+//---------------------------------------------
 
+#include <cstdio>
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -11,8 +15,7 @@
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
+int getch(void); // hole Taste (s.u.)
 
 class TurtleSimPublisher : public rclcpp::Node
 {
@@ -29,13 +32,20 @@ class TurtleSimPublisher : public rclcpp::Node
     {
       // auto => automatischen Speicherklasse, d. h. eine Variable mit lokaler Lebensdauer
       auto msg = geometry_msgs::msg::Twist();  // instanziert Twist - Object 
-      
+
+      // Hole Taste
+      char key = getch();
+      double speed_val = 0.0;
+
       // Setze Werte für die Message
       double speed_val = 0.2;
       msg.linear.set__x(speed_val);
 
+      /****** HIER CODE EINFÜGEN ******/
+
       cmd_pub_->publish(msg);   // Sende Nachricht ins ROS2
       RCLCPP_INFO(this->get_logger(), "Publishing linear X : '%f'", msg.linear.x); //Konsolenausgabe
+      RCLCPP_INFO(this->get_logger(), "Publishing angular Z : '%f'", msg.angular.z); //Konsolenausgabe
     }
   
   private:  //Klassenvariablen
@@ -44,10 +54,46 @@ class TurtleSimPublisher : public rclcpp::Node
     size_t count_;
 };
 
+
+
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
+  std::cout<<  " Tasten-Steurung für die TurtleSim - Druecken Sie 'W' 'A' 'S' 'D' "; //Konsolenausgabe
   rclcpp::spin(std::make_shared<TurtleSimPublisher>());
   rclcpp::shutdown();
   return 0;
+}
+
+// https://raw.githubusercontent.com/aarsht7/teleop_cpp_ros2/main/src/teleop_cpp_ros2.cpp
+// For non-blocking keyboard inputs
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+int getch(void)
+{
+  int ch;
+  struct termios oldt;
+  struct termios newt;
+
+  // Store old settings, and copy to new settings
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+
+  // Make required changes and apply the settings
+  newt.c_lflag &= ~(ICANON | ECHO);
+  newt.c_iflag |= IGNBRK;
+  newt.c_iflag &= ~(INLCR | ICRNL | IXON | IXOFF);
+  newt.c_lflag &= ~(ICANON | ECHO | ECHOK | ECHOE | ECHONL | ISIG | IEXTEN);
+  newt.c_cc[VMIN] = 1;
+  newt.c_cc[VTIME] = 0;
+  tcsetattr(fileno(stdin), TCSANOW, &newt);
+
+  // Get the current character
+  ch = getchar();
+
+  // Reapply old settings
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+  return ch;
 }
