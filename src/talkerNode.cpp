e46e96184106fee660e69d945d1a413b9c3c3036
ProfJust <olaf.just@w-hs.de>
2024-03-11 10:06:32 +0100

    mit CheatSheet

--- src/talkerNode.cpp
+++ src/talkerNode.cpp
@@ -5,6 +5,6 @@ int main(int argc, char ** argv)
   (void) argc;
   (void) argv;
 
-  printf("hello world emr2 package in SoSe24\n");
+  printf("hello world emr2 package in SoSe24\n"); 
   return 0;
 }
