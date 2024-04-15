ecb952852b6327a3c50f40dde7c868951e5825f0
ProfJust <olaf.just@w-hs.de>
2024-04-15 16:17:45 +0200

    -y

--- install/step1_Install_ROS2_Hawskbill.sh
+++ install/step1_Install_ROS2_Hawskbill.sh
@@ -25,8 +25,8 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 # Install ROS 2 packages
-sudo apt update 
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt install ros-humble-desktop -y
 sudo apt install ros-humble-ros-base -y
 sudo apt install ros-dev-tools -y
