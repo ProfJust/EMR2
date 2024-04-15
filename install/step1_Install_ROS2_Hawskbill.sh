a7e7f71962e464ed9a49761bf72081828939726e
ProfJust <olaf.just@w-hs.de>
2024-04-15 13:01:11 +0200

    ohne colcon build

--- install/step1_Install_ROS2_Hawskbill.sh
+++ install/step1_Install_ROS2_Hawskbill.sh
@@ -79,7 +79,7 @@ rosdep update
 
 source ~/.bashrc
 cd ~/ur3_ws/
-colcon build --symlink-install
+# colcon build --symlink-install
 # wenn schwacher PC nur mit 1 Kern kompilieren =>
 # colcon build --symlink-install --parallel-workers 1
 # source ~/turtlebot3_ws/install/local_setup.bash
