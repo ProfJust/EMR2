5dd5e799d4f9bfb2a82c9d1a7ecfcbdd4e26ad6b
ProfJust <olaf.just@w-hs.de>
2024-06-25 10:39:46 +0200

    new humble

--- install/step3_Install_Moveit2_Tutorials.sh
+++ install/step3_Install_Moveit2_Tutorials.sh
@@ -2,19 +2,14 @@
 
 sudo apt install python3-rosdep
 
-sudo rosdep init
-rosdep update
-sudo apt update -y
-sudo apt dist-upgrade -y
-
 # Install Colcon the ROS 2 build system with mixin:
 
-sudo apt install python3-colcon-common-extensions
-sudo apt install python3-colcon-mixin
+sudo apt install python3-colcon-common-extensions -y
+sudo apt install python3-colcon-mixin -y
 colcon mixin add default https://raw.githubusercontent.com/colcon/colcon-mixin-repository/master/index.yaml
 colcon mixin update default
 
-sudo apt install python3-vcstool
+sudo apt install python3-vcstool -y
 
 cd ~/ur3_ws/src
 git clone --branch humble https://github.com/ros-planning/moveit2_tutorials
@@ -32,7 +27,28 @@ colcon build --mixin release   # nur so compiliert es, ohne mixin => fail
 # echo 'alias ce='colcon build --packages-select emr2' >> ~/.bashrc
 
 # change to Cyclone DDS 
-sudo apt install ros-humble-rmw-cyclonedds-cpp -y
-echo 'export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp' >> ~/.bashrc  # Ergänze .bashrc
+# sudo apt install ros-humble-rmw-cyclonedds-cpp -y
+sudo apt install ros-humble-moveit-msgs -y
+sudo apt install ros-humble-ros-testing -y
+sudo apt install ros-humble-eigen-stl-containers -y
+sudo apt install ros-humble-geometric-shapes -y
+sudo apt install ros-humble-ros2-control-test-assets -y
+sudo apt install ros-humble-ruckig -y
+sudo apt install ros-humble-ur-msgs -y
+sudo apt install ros-humble-controller-manager -y
+sudo apt install ros-humble-ur-client-library -y
+sudo apt install ros-humble-graph-msgs -y
+sudo apt install ros-humble-ompl -y
+sudo apt install ros-humble-warehouse-ros -y
+sudo apt install ros-humble-rviz-visual-tools -y
 
-ros2 launch moveit2_tutorials demo.launch.py rviz_config:=your_rviz_config.rviz
+sudo apt update -y
+sudo apt dist-upgrade -y
+sudo rosdep init
+rosdep update
+
+
+
+#echo 'export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp' >> ~/.bashrc  # Ergänze .bashrc
+
+# ros2 launch moveit2_tutorials demo.launch.py rviz_config:=your_rviz_config.rviz
